76a2e150dc31c7d4bbf3ce0daa6695eff287f965
safira123-pixel <safiraistifarini@gmail.com>
2022-09-18 15:39:15 +0700

    remove comment

--- lib/main.dart
+++ lib/main.dart
@@ -7,21 +7,21 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        
+        
+        
+        
+        
+        
+        
+        
+        
         primarySwatch: Colors.red,
       ),
       debugShowCheckedModeBanner: false,
@@ -33,14 +33,14 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  
+  
+  
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  
+  
+  
+  
 
   final String title;
 
@@ -178,7 +178,7 @@ class _MyHomePageState extends State<MyHomePage> {
                         child: Container(
                           width: MediaQuery.of(context).size.width,
                           padding: const EdgeInsets.all(0.0),
-                          // child: Image.asset("assets/images/foto2.jpeg"),
+                          
                           child: Image.asset("assets/foto2.jpg"),
                         ),
                       ),
